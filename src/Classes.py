ae70d9f48fbbd91b5972370f9ef624cb8f2a9e31
Zeroks77 <dominik@mangatter.de>
2019-07-12 09:03:34 +0200

    init commit

---
+++ src/Classes.py
@@ -0,0 +1,66 @@
+
+
+
+class Weapon:
+
+    def __init__(self, name, kind, dmg, worth, broken):
+        self.name = name
+        self.kind = kind
+        self.dmg = dmg
+        self.worth = worth
+        self.broken = broken
+
+    def can_attack(self):
+        if self.broken == False:
+            return True
+        else:
+            return False
+
+class Character:
+
+    def __init__(self, name, hp, mana):
+        self.name = name
+        self.hp = hp
+        self.mana = mana
+
+    def can_do_action(self):
+        if self.hp > 0:
+            print("Choose how you want to continue!")
+        else:
+            print("You can't do anything, your character is dead!")
+
+
+class Item:
+
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
+
+class Room:
+
+    def __init__(self, size, props, loot, enemies):
+        self.size = size
+        self.props = props
+        self.loot = loot
+        self.enemies = enemies
+
+class BigRoom(Room):
+    def __init__(self):
+        super().__init__(size="big", props="chairs", loot="gold", enemies="spiders")
+
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class StartingRoom(MapTile):
+    def intro_text(self):
+        return """
+        You find yourself if a cave with a flickering torch on the wall.
+        You can make out four paths, each equally as dark and foreboding.
+        """
+
+    def modify_player(self, player):
+        # Room has no action on player
+        pass
---
+++ src/Text_Adventure.py
@@ -0,0 +1,134 @@
+# name: Richard
+# date: 08-07-2019
+# description: text-based adventure game
+import random
+import time
+import sys
+
+from Classes import Room
+
+############### Changes to be made ###############
+#
+# input auf upper and lower
+#
+#
+#
+#
+#
+#
+#
+#
+##################################################
+
+
+
+def displayIntro():
+    print("#############################################")
+    print("##                                         ##")
+    print("##                WELCOME                  ##")
+    print("##          TO THE ADVENTURES OF           ##")
+    print("##          GERWALD OF WALDSTETT           ##")
+    print("##                                         ##")
+    print("##          Type: START to start           ##")
+    print("##       Type: HELP to receive help        ##")
+    print("##       Type: QUIT to quit the game       ##")
+    print("##  Type: LANGUAGE to change the language  ##")
+    print("##                                         ##")
+    print("#############################################")
+
+
+displayIntro()
+
+
+def IntroQuestion(intro):
+    if intro == "START" or intro == "start":
+        print("\n> Alright, so let us start the adventure of Gerwald of Waldstett!")
+        #time.sleep(1.5)
+        return Story(intro)
+    elif intro == "HELP" or intro == "help":
+        print("\nINSTRUCTIONS, just follow along the questions and just input the things you are allowed to!!!")
+        #time.sleep(1.5)
+        return Help(intro)
+    elif intro == "QUIT" or intro == "quit":
+        print("\n#######################################")
+        print("##                                   ##")
+        print("##  Thank you for playing the game.  ##")
+        print("##                                   ##")
+        print("#######################################")
+        sys.exit()
+    else:
+        intro = str(input("\nPlease enter one of the commands above: "))
+        return IntroQuestion(intro)
+
+
+def Story(intro):
+    if intro == "START" or intro == "start":
+        #time.sleep(1.5)
+        print("\n> I hope you brought enough time and i hope you have fun playing.")
+        return
+    else:
+        intro = str(input("\nPlease enter a valid command! "))
+        return IntroQuestion(intro)
+
+def Help(intro):
+    if intro == "HELP" or intro == "help":
+        intro = input("\nIf you are ready to start the game, enter the START command! ")
+        return IntroQuestion(intro)
+    else:
+        intro = str(input("\nPlease enter a valid command! "))
+        return IntroQuestion(intro)
+
+intro = str(input("\n> Please state what you want to do! \n"))
+
+IntroQuestion(intro)
+
+
+#time.sleep(1.5)
+print("\n> And so the tale begins. Years ago in the land of Ghrezok, there lived a man named Gerwald in a city called Waldstett.")
+#time.sleep(1.5)
+print("\n> You now have the choice of choosing one of two paths. Type (1) to choose path one or (2) to choose path two.")
+
+
+def Path(choose_path):
+    if choose_path == "1":
+        print("\n> Alright, you chose the first path")
+        return 1
+    elif choose_path == "2":
+        print("\n> You chose path 2")
+        return 2
+    else:
+        choose_path = str(input("> Pleas enter a valid command! "))
+        return Path(choose_path)
+
+choose_path = str(input("\n> Enter your choice: "))
+
+Path(choose_path)
+
+
+def Path1(action):
+    if action == "LOOK" or action == "look":
+        print("> You are looking around")
+        return
+    elif action == "TAKE" or action == "take":
+        print("> There is nothing to take yet.")
+        return
+    elif action == "ATTACK" or action == "attack":
+        print("> There is nothing to attack yet.")
+        return
+    else:
+        action = str(input("> Please enter a valid command! "))
+        return Path1(action)
+
+
+action = str(input("\n> What do you want to do, you can LOOK, TAKE or ATTACK: "))
+
+
+while Path(choose_path) == 1:
+    Path1(action)
+    action = str(input("\n> What do you want to do, you can LOOK, TAKE or ATTACK: "))
+    if action == "leave" or action == "LEAVE":
+        break
+
+
+
+print("well done, you broke our loop")
