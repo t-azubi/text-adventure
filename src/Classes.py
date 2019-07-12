9c36881a1637896e6a6edf30881bc963999ae5fc
Richard Meusel <richard.meusel@telekom.de>
2019-07-12 15:00:23 +0200

    löschen unnötiger klassen

--- src/Classes.py
+++ src/Classes.py
@@ -1,3 +1,5 @@
+import enum
+
 
 class quotes :
    def invaildCommand(self):
@@ -10,7 +12,6 @@ class quotes :
      TAKE = 3
 
 class Weapon:
-
     def __init__(self, name, kind, dmg, worth, broken):
         self.name = name
         self.kind = kind
@@ -25,7 +26,6 @@ class Weapon:
             return False
 
 class Character:
-
     def __init__(self, name, hp, mana):
         self.name = name
         self.hp = hp
@@ -39,35 +39,18 @@ class Character:
 
 
 class Item:
-
     def __init__(self, name, kind):
         self.name = name
         self.kind = kind
 
-class Room:
 
+class Room:
     def __init__(self, size, props, loot, enemies):
         self.size = size
         self.props = props
         self.loot = loot
         self.enemies = enemies
 
-class BigRoom(Room):
-    def __init__(self):
-        super().__init__(size="big", props="chairs", loot="gold", enemies="spiders")
-
-class MapTile:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-class StartingRoom(MapTile):
-    def intro_text(self):
-        return """
-        You find yourself if a cave with a flickering torch on the wall.
-        You can make out four paths, each equally as dark and foreboding.
-        """
 
     def modify_player(self, player):
         # Room has no action on player
