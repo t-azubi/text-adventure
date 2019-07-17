c9f406f513d54bc8801327e61e82025b782cd04b
Richard Meusel <richard.meusel@telekom.de>
2019-07-17 12:25:04 +0200

    erste idee merchant, gegenchecken pls

--- src/merchant.py
+++ src/merchant.py
@@ -1,4 +1,5 @@
-
+from Player import Character
+from items import Item
 
 
 class Merchant:
@@ -17,6 +18,22 @@ class Merchant:
 merchant_items = ["sword", "axe", "hammer"]
 
 
+########
+if Merchant.gold > 0:
+    print("You can sell items to the merchant.")
+elif Merchant.gold == 0:
+    print("You cant sell items to the merchant.")
+
+if Character.inventory. > 0  ### zugriff auf gold des characters ?
+    player can buy items from merchant
+    elif
+    player gold == 0
+    player cant buy item
+########
+
+selling von items dann durch appenden der merchant item list
+
+zuweisen von values zu den items in der liste ?
 
 
 ##player selling items
